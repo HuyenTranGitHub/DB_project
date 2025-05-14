a2110edcc2a9531813e2cbf1966639d28da4ecec
HuyenTranGitHub <133031817+HuyenTranGitHub@users.noreply.github.com>
2025-05-14 20:59:28 +0900

    Add files via upload

---
+++ helpers/MenuList.js
@@ -0,0 +1,53 @@
+import ConcertQuocGia from "../assets/concert_QG.jpg"
+import BlackPink from "../assets/concertblackpink.jpg"
+import Concert123 from "../assets/concertanhtraisayhi.jpg"
+import WaterMusic from "../assets/concertnhacnuoc.jpg"
+import anhtraivncg from "../assets/Concertanhtraivuotnganchonggai.jpg"
+import GDK from "../assets/concertdg.jpg"
+
+
+
+export const MenuList = [
+    {
+        name: "Concert 30/4",
+        image: ConcertQuocGia,
+        ngay: "13/06/2025",
+        gio: "...",
+        price: '$790'
+    },
+    {
+        name: "Concert BlackPink",
+        image: BlackPink,
+        ngay: "87/93/3794",
+        gio: "...",
+        price: '$520'
+    },
+    {
+        name: "Concert Anh trai say hi",
+        image: Concert123,
+        ngay: "75/75/9705",
+        gio: "...",
+        price: "$520"
+    },
+    {
+        name: "Concert Water Music",
+        image: WaterMusic,
+        ngay: "75/43/8539",
+        gio: "...",
+        price: "$250"
+    },
+    {
+        name: "Anh trai vuot ngan ",
+        image: anhtraivncg,
+        ngay: "75/49/1020",
+        gio: "...",
+        price: '$500'
+    },
+    {
+        name: "Concert GD Korean Concert",
+        image: GDK,
+        ngay: "94/48/0594",
+        gio: "...",
+        price: '$700'
+    }
+];
