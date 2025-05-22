8fc7ec49ed389e3eac43624dab67e62934aedcee
HuyenTranGitHub <133031817+HuyenTranGitHub@users.noreply.github.com>
2025-05-22 16:25:07 +0900

    Navbar.js

--- component/Navbar.js
+++ component/Navbar.js
@@ -1,17 +1,29 @@
 //gõ rfce để ra từ 2-12: react functional component export
 import React, { useState } from 'react'
 import logo from '../assets/logoconcert.png'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import '../styles/Navbar.css'
 import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
 
     const [openLinks, setOpenLinks] = useState(false);
+    const [showUserMenu, setShowUserMenu] = useState(false); // dòng này cho dropdown các mục trong khi nhấp vào nút email
+    const navigate = useNavigate();
 
     const toggleNavbar = () => {
         setOpenLinks(!openLinks)
+    };
+    const user = JSON.parse(localStorage.getItem('user'));
+    const handleLogout = () => {
+        localStorage.removeItem('user');
+        navigate('/login');
     }
+
+    const handleProfileClick = () => {
+        navigate('/signup'); // Trang signup sẽ hiển thị ở chế độ profile nếu user đã login
+    };
+
 
     return (
         <div className="navbar">
@@ -22,15 +34,33 @@ function Navbar() {
                     <Link to="/menu">Menu</Link>
                     <Link to="/about">About</Link>
                     <Link to="/contact">Contact</Link>
-                    <Link to="/login">Login</Link>
+                    {!user && <Link to="/login">Login</Link>}
                 </div>
             </div>
+
             <div className='rightSide'>
                 <Link to="/">Home</Link>
                 <Link to="/menu">Menu</Link>
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact</Link>
-                <Link to="/login">Login</Link>
+                {user ? (
+                    <div className='user-menu'>
+                        <span
+                            className='user-email'
+                            onClick={() => setShowUserMenu(!showUserMenu)}
+                        >
+                            {user.email}
+                        </span>
+                        {showUserMenu && (
+                            <div className="dropdown-menu">
+                                <div onClick={handleProfileClick}>Thong tin khach hang</div>
+                                <div onClick={handleLogout}>Logout</div>
+                            </div>
+                        )}
+                    </div>
+                ) : (
+                    <Link to="/login">Login</Link>
+                )}
                 <button onClick={toggleNavbar}>
                     <ReorderIcon />
                 </button>
